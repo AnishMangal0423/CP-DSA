acdde632bd499afa0dec04aec97c2a736723f6ed
anish mangal <anishm0423@gmail.com>
2024-01-18 14:51:05 +0530

    FEAT : Added Right/Left View of Binary Tree

---
+++ src/DSA/Tree/Right_View.java
@@ -0,0 +1,54 @@
+package DSA.Tree;
+
+//import java.util.ArrayList;
+//import java.util.LinkedList;
+import java.util.*;
+
+class RightViewPair{
+
+    TreeNode node;
+    int level;
+
+    RightViewPair(TreeNode node, int l){
+        this.node=node;
+        this.level=l;
+    }
+}
+public class Right_View{
+
+
+    public static ArrayList<Integer> View(TreeNode root){
+        ArrayList<Integer>ans=new ArrayList<>();
+        if(root!=null){
+            Queue<RightViewPair>q=new LinkedList<>();
+            q.add(new RightViewPair(root, 0));
+            int start=-1;
+
+            while (!q.isEmpty()){
+
+                RightViewPair front = q.poll();
+                TreeNode node = front.node;
+                int level=front.level;
+
+                if(start!=level){
+                    start=level;
+                    ans.add(node.data);
+                }
+
+                if(node.right!=null)q.add(new RightViewPair(node.right, level+1));
+                if(node.left!=null )q.add(new RightViewPair(node.left, level+1));
+
+
+            }
+
+        }
+        return ans;
+    }
+    public static void main(String[] args) {
+
+        TreeNode root= Tree.CreateTree();
+
+        System.out.println(View(root));
+
+    }
+}
