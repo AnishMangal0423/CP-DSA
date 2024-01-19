16d78dee9a18b41eae3994fe42e6c19172822c13
anish mangal <anishm0423@gmail.com>
2024-01-19 16:46:58 +0530

    FEAT : Number Theory Fcatorization Question

--- src/TLE_LVL_2/Number_Theory/Number_Theory_1/E_K_Factorization.java
+++ src/TLE_LVL_2/Number_Theory/Number_Theory_1/E_K_Factorization.java
@@ -3,6 +3,12 @@ package TLE_LVL_2.Number_Theory.Number_Theory_1;
 import java.util.ArrayList;
 import java.util.Scanner;
 
+/**
+ *
+ * Main thing in this was to Vizualize that it was a Qestion from Number Theory.
+ * Whenever Fcatorization Comes Think on Number Theory .. for 1100 Rating Question.
+ * **/
+
 public class E_K_Factorization{
 
     public static void main(String[] args) {
@@ -13,24 +19,25 @@ public class E_K_Factorization{
         int k=sc.nextInt();
 
         ArrayList<Integer>ans=new ArrayList<>();
+
+        int kold=k;
         for(int i=2; i*i<=n; i++){
 
-            if(n%i==0){
-                while (n%i==0){
-                 n/=i;
-                 k--;
-                 ans.add(i);
-                 if(k==1)break;
-                }
-            }
+              if(n%i==0){
+
+                  while (n%i==0 && k > 1){
+                      n/=i;
+                      ans.add(i);
+                      k--;
+                  }
+              }
         }
 
-        if((n>1 && k==1) || (n==1 && k==0)){
-            ans.add(n);
-            for(int i : ans) System.out.print(i);
-        }
-        else System.out.println(-1);
+        if(k==1 && n>1)ans.add(n);
 
+
+        if(ans.size()!=kold) System.out.println(-1);
+        else  for(int i : ans) System.out.print(i+" ");
 
     }
 }
