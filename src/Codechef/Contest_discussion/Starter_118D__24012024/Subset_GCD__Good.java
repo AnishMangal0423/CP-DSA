54148585ab8a0bcbd87023c0463dae2552ec27ee
anish mangal <anishm0423@gmail.com>
2024-01-25 22:02:30 +0530

    Learn the Observation,, Good Question

--- src/Codechef/Contest_discussion/Starter_118D__24012024/Subset_GCD__Good.java
+++ src/Codechef/Contest_discussion/Starter_118D__24012024/Subset_GCD__Good.java
@@ -2,7 +2,7 @@ package Codechef.Contest_discussion.Starter_118D__24012024;
 
 import java.util.Scanner;
 
-public class Q6 {
+public class Subset_GCD__Good {
     public static void main(String[] args) {
 
         Scanner sc=new Scanner(System.in);
@@ -12,9 +12,15 @@ public class Q6 {
         while (tc-- >0){
 
             int n=sc.nextInt();
+            int k=sc.nextInt();
 
-//            int[]
+            int G=1;
+            while (k*G <= n) G++;
 
+           for(int i=1; i<=k; i++){
+               System.out.print((G-1)*i);
+           }
+            System.out.println(" ");
         }
     }
 }
