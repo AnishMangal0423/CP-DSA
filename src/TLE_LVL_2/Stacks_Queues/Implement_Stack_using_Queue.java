7a683607bb30d35f150ac9ecb7fdd1ec5cc234f2
anish mangal <anishm0423@gmail.com>
2024-02-04 19:32:36 +0530

    Implemented By using Single Queue

---
+++ src/TLE_LVL_2/Stacks_Queues/Implement_Stack_using_Queue.java
@@ -0,0 +1,90 @@
+package TLE_LVL_2.Stacks_Queues;
+
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+
+/**
+ *
+ * Approch -1 : Two Queues : Take Proper O(N) time to swap and push elemnts and O(1) time for pop and peek.
+ *
+ *               TC - O(N^2) for push but always tell for 1 elements so O(N) .
+ *               SC - O(~N)
+ *
+ *
+ *
+ *
+ * Approch-2 : One Queue : Remove first size-1 elements and to last of queue.
+ *
+ *        TC - (SAME)
+ *        SC - O(N)
+ * **/
+
+
+
+class MyStack{
+
+    Queue<Integer>q;
+
+    MyStack(){
+        q=new LinkedList<>();
+    }
+
+    void push(int x){
+        q.add(x);
+    }
+
+    boolean isEmpty(){
+        return q.size()==0 ? true : false;
+    }
+
+
+    int peek(){
+
+        int size = q.size();
+
+        if(size==0)return -1;
+
+        for(int i=0; i<size-1; i++){
+            q.add(q.poll());
+        }
+
+        return q.peek();
+    }
+
+
+
+    int pop(){
+        int size = q.size();
+
+        if(size==0)return -1;
+
+        for(int i=0; i<size-1; i++){
+            q.add(q.poll());
+        }
+
+        return q.poll();
+
+    }
+
+}
+public class Implement_Stack_using_Queue{
+
+    public static void main(String[] args) {
+
+        MyStack stk = new MyStack();
+
+        System.out.println(stk.isEmpty());
+        stk.push(10);
+        stk.push(20);
+        stk.push(30);
+        stk.push(40);
+
+        System.out.println(stk.pop());
+        System.out.println(stk.peek());
+        System.out.println(stk.isEmpty());
+
+
+    }
+}
