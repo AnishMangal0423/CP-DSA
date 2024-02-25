7d3726711922b8eaf675ac10ed737843ae3f72a5
anish mangal <anishm0423@gmail.com>
2024-02-25 17:05:50 +0530

    Add : Learned How to Calculate the Area of Insersecting Figures (Coordinates Manupulation)

--- src/LeetCode/Contest/Weekly_386_25022024/Q2.java
+++ src/LeetCode/Contest/Weekly_386_25022024/Q2.java
@@ -5,20 +5,46 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.Scanner;
 
+/**
+ *
+ * Concept --> Calculate the Area of Intresection of Many Squares.
+ *
+ * Learning --> Understanded , How to Deal with Finding the Intersection Points of Figures ( Use the Concept of min/max Bundries and
+ *              and make the condition that in actual gap(area/intresection) is Created or not.
+ *
+ *
+ * Time Complexity  --> O(N^2) 2 Loops.
+ * Space Complexity --> O(1) Constant Space.
+ *
+ *
+ * **/
 public class Q2 {
 
     public static void main(String[] args) {
 
-//        Scanner sc = new Scanner(System.in);
-//
-//
-//        int side = 0;
-//        int maxi = Integer.MAX_VALUE;
-//
-//        int n = bl.length;
-//        ArrayList<Integer>mm = new ArrayList<>();
-//        for(int i=0; i<n; i++){
-//
+        Scanner sc = new Scanner(System.in);
+        int n_Squares = sc.nextInt();
+
+        int[][]bottomLeft = new int[n_Squares][2];
+        int[][]topRight = new int[n_Squares][2];
+
+        for(int i=0; i<n_Squares; i++){
+            for(int j=0; j<2; j++){
+                bottomLeft[i][j]=sc.nextInt();
+            }
+        }
+
+        for(int i=0; i<n_Squares; i++){
+            for(int j=0; j<2; j++){
+                topRight[i][j]=sc.nextInt();
+            }
+        }
+
+        int side = 0;
+
+        for(int i=0; i<n_Squares; i++){
+
+// ------------------------------ Method I used During Contest ---------------------------------------------------------
 //            int sx1 = bl[i][0];
 //            int ex1 = tr[i][0];
 //            int sy1 = bl[i][1];
@@ -49,13 +75,36 @@ public class Q2 {
 //
 //
 //                mm.add(Math.min(val1 , val2));
-//
-//            }
-//        }
-//
-//        Collections.sort(mm);
-//
-//        return; mm.get(0);
+
+// ----------------------------------- Lengthy and Not Good Practice ----------------------------------------------------
+
+
+
+// ---------------------------------- Good and Correct Practice --------------------------------------------------------
+
+
+            // I will find the bottomLeft coord1 , TopRight coord2 of Insertected Square -->
+
+            for(int j= i+1; j<n_Squares; j++) {
+
+                int coord1x = Math.max(bottomLeft[i][0] , bottomLeft[j][0]);   // It is BootomLeft x new Coordinate of New Intresected Square.
+                int coord1y = Math.max(bottomLeft[i][1] , bottomLeft[j][1]);   // It is BootomLeft y new Coordinate of New Intresected Square.
+
+
+                int coord2x = Math.min(topRight[i][0] , topRight[j][0]);     // It is topRight x new Coordinate of New Intresected Square.
+                int coord2y = Math.min(topRight[i][1] , topRight[j][1]);     // It is topRight y new Coordinate of New Intresected Square.
+
+                // It is necessary condition to remove all Mutually Exculsive Cases.
+                if(coord1x < coord2x && coord1y < coord2y){
+
+                    side = Math.max(side , Math.min(coord2x - coord1x , coord2y - coord1y));
+                }
+
+
+            }
+        }
+
+        System.out.println(side*side);
 
     }
 }
