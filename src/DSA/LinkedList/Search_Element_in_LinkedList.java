00ef72688ffc89e1902eb4a0c2ec04eaf0b9a88d
anish mangal <anishm0423@gmail.com>
2024-03-02 11:17:18 +0530

    Add : Search key in LL

---
+++ src/DSA/LinkedList/Search_Element_in_LinkedList.java
@@ -0,0 +1,30 @@
+package DSA.LinkedList;
+
+import java.util.List;
+import java.util.Scanner;
+
+public class Search_Element_in_LinkedList{
+
+    public  static boolean isPresent(ListNode list, int k){
+
+        ListNode temp = list;
+        while (temp!=null){
+            if(temp.data==k)return true;
+            temp=temp.next;
+        }
+
+        return false;
+    }
+    public static void main(String[] args) {
+
+        ListNode list = LinkedList.CreateLinkedList();
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the Key to search in LinkedList : ");
+
+        int k = sc.nextInt();
+
+        System.out.println("Is Key Present in LinkedList : "+ isPresent(list, k));
+
+    }
+}
