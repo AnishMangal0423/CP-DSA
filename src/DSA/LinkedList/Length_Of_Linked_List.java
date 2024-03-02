670fce59628a008f353d9e4abf35fd72c4dd5b65
anish mangal <anishm0423@gmail.com>
2024-03-02 11:13:28 +0530

    Add : Length of ll

---
+++ src/DSA/LinkedList/Length_Of_Linked_List.java
@@ -0,0 +1,22 @@
+package DSA.LinkedList;
+
+public class Length_Of_Linked_List{
+
+    public static int length(ListNode list){
+
+        ListNode temp = list;
+
+        int len = 0;
+        while (temp!=null){
+            temp=temp.next;
+            len++;
+        }
+        return len;
+    }
+    public static void main(String[] args) {
+
+        ListNode list = LinkedList.CreateLinkedList();
+
+        System.out.println("Length of LinkedList is : "+ length(list));
+    }
+}
