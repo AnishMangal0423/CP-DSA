da5fa1fb106f9b2db44e9fa267a2a5876ac36bcf
anish mangal <anishm0423@gmail.com>
2024-03-04 08:54:38 +0530

    Same Variation of Lower Bound of a Key and array

---
+++ src/DSA/BinarySearch/Search_Insert_Position.java
@@ -0,0 +1,38 @@
+package DSA.BinarySearch;
+
+import java.util.Scanner;
+
+public class Search_Insert_Position{
+
+    public static int BinarySearch(int[]arr, int Key){
+
+        int n = arr.length;
+        int low = 0;
+        int high = n-1;
+
+        while (low <= high){
+
+            int mid = (low + high)/2;
+
+            if(arr[mid]==Key) return mid;
+            else if(arr[mid] > Key){
+                high = mid-1;
+            }
+            else low = mid + 1;
+
+        }
+        return low;
+    }
+    public static void main(String[] args) {
+
+        System.out.println("Enter the Size of Array : ");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[]arr = new int[n];
+        for(int i=0; i<n; i++)arr[i]=sc.nextInt();
+        System.out.println("Enter the key to search  : ");
+        int Key = sc.nextInt();
+
+        System.out.println("Index of Inserting Key : " + BinarySearch(arr, Key));
+    }
+}
