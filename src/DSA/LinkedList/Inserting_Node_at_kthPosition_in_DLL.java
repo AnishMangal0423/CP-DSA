4b879ad4a4ad9a71bb947be050696d9904cf66c0
anish mangal <anishm0423@gmail.com>
2024-03-07 16:47:21 +0530

    Add : Insertion in dll

---
+++ src/DSA/LinkedList/Inserting_Node_at_kthPosition_in_DLL.java
@@ -0,0 +1,55 @@
+package DSA.LinkedList;
+
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class Inserting_Node_at_kthPosition_in_DLL{
+
+    public static void DisplayDLL(DoublyListNode list){
+
+        DoublyListNode temp = list;
+        while (temp!=null){
+            System.out.print(temp.data +", ");
+            temp=temp.next;
+        }
+        System.out.println("null");
+    }
+    public static DoublyListNode Insert(DoublyListNode list, int k){
+
+        System.out.println("Enter the Data of the New Node");
+        Scanner sc = new Scanner(System.in);
+        int data = sc.nextInt();
+
+        DoublyListNode newNode = new DoublyListNode(data);
+        DoublyListNode temp = list;
+        DoublyListNode prev = null;
+
+        while (k-->0){
+            prev=temp;
+            temp=temp.next;
+        }
+
+        newNode.next=temp;
+        temp.prev=newNode;
+
+        if(prev==null)return newNode;
+        prev.next=newNode;
+        newNode.prev=prev;
+
+        return list;
+
+
+    }
+    public static void main(String[] args) {
+
+        DoublyListNode list = DoublyLinkedList.CreateDLL();
+        System.out.println("Enter the position where we want to Insert the Node..");
+
+        Scanner sc = new Scanner(System.in);
+        int k = sc.nextInt();
+
+        DoublyListNode NewList = Insert(list, k);
+        DisplayDLL(NewList);
+
+    }
+}
