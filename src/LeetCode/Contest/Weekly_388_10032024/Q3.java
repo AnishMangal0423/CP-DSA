09cf94b73a84ff877142e6b8145eedc451999b58
anish mangal <anishm0423@gmail.com>
2024-03-10 18:11:35 +0530

    Concepts : Finding Lexicographically smallest string , Find substrings , Manupulation with map, string

--- src/LeetCode/Contest/Weekly_388_10032024/Q3.java
+++ src/LeetCode/Contest/Weekly_388_10032024/Q3.java
@@ -1,9 +1,36 @@
 package LeetCode.Contest.Weekly_388_10032024;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Scanner;
+import java.util.*;
+
+/***
+ *
+ *
+ * Question :- Ek Strings ki array given h , aur abb hme kya karna h ki string[]ans hi return krna h .. ki ans[i] = "" ya to
+ *             Null hogi ya fir lexcicographically smallest substring hogi arr[i] ki ho bakki kisi bhi strings ke substring
+ *             mai mentioned nhi h.
+ *
+ *
+ *
+ *  Approch :- Simple But lengthy-
+ *             1. Har string pe jao , bakki ka map bna lo.
+ *             2. aab is string har substring ko check krlo h ya nhi.
+ *             3. agar nhi h , to lexicographically smallest ka loop laga do.
+ *             4. Ans aa jayega.
+ *
+ *
+ *
+ *             TC - O(N* M^2 * log(M^2))  ~~ 10^5 becausse n <= 100 && m<=20
+ *             SC - O(N * M * M ) of hashmap at worst casse.
+ *
+ *
+ *
+ * Larning :- It was an easy question, but a lengthy one.
+ *            1. Understand question properly.
+ *            2. Make map in mind ( becuase it is lengthy so it will be worse if you change in between )
+ *            3. Implement with calm mind and fast hands, not opoosite.
+ *
+ * **/
+
 
 public class Q3{
     public static void main(String[] args) {
@@ -12,29 +39,14 @@ public class Q3{
 
         int n = sc.nextInt();
         String[]arr=new String[n];
+        for(int i=0; i<n; i++)arr[i]=sc.next();
 
-        HashMap<String, Integer>map = new HashMap<>();
-
-        for(String s : arr){
-
-            for(int i=0; i<s.length(); i++){
-
-                String aa = "" + s.charAt(i);
-                for(int j=i+1; j<s.length(); j++){
-
-                      aa +=s.charAt(j);
-                    if(map.containsKey(aa)==false){
-                        map.put(aa, 1);
-                    }
-                }
-            }
-        }
-
-
-
+        String[]ans = new String[n];
 
         for(int i=0; i<n; i++){
 
+
+            // For every String Making the Map of all other strings :- TC - O(N * M^2 log(M^2)) , SC - O(N*M^2)
             HashMap<String , Integer>mm = new HashMap<>();
             for(int j=0; j<n; j++){
 
@@ -43,13 +55,13 @@ public class Q3{
 
                     String ss = arr[j];
 
+                    // Calcultaing all the Substrings of a string -
                     for(int l=0; l<ss.length(); l++){
-//                        String aa = "" + ss.charAt(i);
 
-                        for(int m=l+1; m<=ss.length(); m++){
-
-                            String  aa = ss.substring(l, m);
-                            mm.put(aa, 1);
+                        String str = "" + ss.charAt(l);
+                        for(int m=l; m<ss.length(); m++){
+                           if(l!=m)str+=ss.charAt(m);
+                           mm.put(str, 1);
                         }
 
                     }
@@ -57,41 +69,36 @@ public class Q3{
                 }
             }
 
-            System.out.println(mm);
-
-
-            ArrayList<String>aans = new ArrayList<>();
-
             String temp = arr[i];
+
+            // TC - O(M^3)
+
+            String smallest = "zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz";  // Longest string of length > 20
 
             for(int b=0; b<temp.length(); b++){
                 for(int c=b+1; c<=temp.length(); c++){
 
+
                     String timp = temp.substring(b,c);
-                    if(map.containsKey(timp)==false){
-                        aans .add(timp);
+                    if(mm.containsKey(timp)==false){
+
+                        if(timp.compareTo(smallest) < 0){
+                            smallest=timp;
+                        }
                     }
                 }
             }
 
-            Collections.sort(aans);
-            if(aans.size()>0) arr[i]= aans.get(0);
-            else arr[i]="";
+            // System.out.println(aans+ " "+ len);
+            if(smallest.length() <= 20) {
+
+                 ans[i]= smallest;
+
+            }
+            else ans[i]="";
         }
 
-        String[]ans = new String[n];
-
-        for(int i=0; i<n; i++){
-
-           String ss = arr[i];
-
-
-
-        }
-
-
-
-
+        System.out.println(Arrays.toString(ans));
 
     }
 }
--- src/LeetCode/Contest/Weekly_388_10032024/Q4.java
+++ src/LeetCode/Contest/Weekly_388_10032024/Q4.java
@@ -4,6 +4,20 @@ public class Q4 {
 
     public static void main(String[] args) {
 
+        // Gyan of Lexicographiclly smallest finding
 
+        // TC - O(N*M)
+    String[]arr= {"aah", "ah", "anish", "aaah", "aaaaoz"};
+
+      String smallest = arr[0];
+
+      for(int i=1; i<arr.length; i++){
+
+          if(arr[i].compareTo(smallest) < 0){
+              smallest=arr[i];
+          }
+      }
+
+        System.out.println(smallest);
     }
 }
