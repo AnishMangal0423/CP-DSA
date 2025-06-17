cb5977bcd8aacfe1096f0989c8080da2826c7e34
anish.mangal <anishm0423@gmail.com>
2025-06-17 12:58:59 +0530

    Update sample.java

--- src/DSA/Backtracking/sample.java
+++ src/DSA/Backtracking/sample.java
@@ -1,4 +1,13 @@
 package DSA.Backtracking;
 
+import java.util.Scanner;
+
 public class sample {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("1");
+        int n = sc.nextInt();
+        System.out.println(n);
+    }
 }
