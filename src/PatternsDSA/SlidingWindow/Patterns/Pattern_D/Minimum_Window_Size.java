fd9844a190bea35bf77fd79089cc172baf414d22
Anish Mangal <anishm0423@gmail.com>
2026-01-16 09:02:08 +0530

    Pattern -4 : Abstract

---
+++ src/PatternsDSA/SlidingWindow/Patterns/Pattern_D/Minimum_Window_Size.java
@@ -0,0 +1,11 @@
+package PatternsDSA.SlidingWindow.Patterns.Pattern_D;
+
+public class Minimum_Window_Size {
+
+    public static void main(String[] args) {
+
+        /**
+         * This is abstract for now.
+         * **/
+    }
+}
