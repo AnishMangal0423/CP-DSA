201d3378637ac685f7201f3f2aef172099c521d6
Anish Mangal <anishmangal@Anishs-MacBook-Air.local>
2026-01-16 08:11:20 +0530

    Pattern-1 : 2 Poiters & Sliding Window

---
+++ src/PatternsDSA/SlidingWindow/Patterns/Pattern_A/Constant_Window_Size.java
@@ -0,0 +1,39 @@
+package PatternsDSA.SlidingWindow.Patterns.Pattern_A;
+
+import java.util.Scanner;
+
+public class Constant_Window_Size {
+
+    public static int Maxsum(int[]arr, int k){
+
+        int n = arr.length;
+        int l=0, r=0, maxsum=0, sum=0, len=0;  //Step-1
+
+        if(k > n) return -1;
+
+        while(r < n){
+            sum += arr[r];   //Step-2
+
+            if(r-l+1 == k){    // Step-3
+                maxsum = Math.max(maxsum, sum);
+                sum -= arr[l];
+                l++;
+            }
+
+            r++;  // Step-4
+        }
+
+        return  maxsum;
+    }
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[]arr = new int[n];
+        System.out.println("Enter the elements of array :");
+        for(int i=0; i<n; i++)arr[i]=sc.nextInt();
+        System.out.println("Enter the size of subarrays you want to encounter :");
+        int k = sc.nextInt();
+        System.out.println("Subarray with maximum sum of fixed length k is : " + Maxsum(arr, k));
+    }
+}
---
+++ src/PatternsDSA/SlidingWindow/Patterns/Pattern_A/Notes.java
@@ -0,0 +1,29 @@
+package PatternsDSA.SlidingWindow.Patterns.Pattern_A;
+
+public class Notes {
+
+    public static void main(String[] args) {
+
+        /**
+         *
+         * This is the pattern 1 , in which the size of window is fixed and
+         * we have to achieve something like maxsum etc..
+         * so what are the steps would be , rather then you know iterate for all subarrays
+         * as brute force it would be better if we would be iterating to only k size windows.
+         *
+         * so steps to do these types of  qns are -
+         *
+         * Step-1 : Declaration of variables -> l, r, maxsum, etc   --> Declaration Time.
+         *
+         * Step-2 : Use Loop & Calculate sum in the start -> Always Good Practice.  --> Loop & Sum Time.
+         *
+         * Step-3 : Now print ans if calculated sum comes in range etc.  --> Ans Printing Time.
+         *
+         * Step-4 : Move forward -> Shrink and Expand Time.
+         *
+         *
+         *
+         *
+         * **/
+    }
+}
