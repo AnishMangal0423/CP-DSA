784a96cb36e74c40334925fc878589f77c10533c
Anish Mangal <anishm0423@gmail.com>
2026-01-16 09:01:07 +0530

    Pattern-3 : 2 Pointers & Sliding Window

---
+++ src/PatternsDSA/SlidingWindow/Patterns/Pattern_C/Number_of_Subarrays.java
@@ -0,0 +1,14 @@
+package PatternsDSA.SlidingWindow.Patterns.Pattern_C;
+
+public class Number_of_Subarrays {
+    public static void main(String[] args) {
+
+        /**
+         * This pattern includes problems releated to counting of subarray that follow certain condition.
+         *
+         * let's suppose no of subarray that has sum <=k.
+         *
+         * they also use the concept of pattern - 2
+         * **/
+    }
+}
